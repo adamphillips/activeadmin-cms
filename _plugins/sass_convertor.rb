edf745a68f2ecd7bc0878ad8f3e4cbf6661d4037
Adam Phillips <aphillips@scholastic.co.uk>
2012-11-04 13:43:09 +0000

    Added sass convertor script
    Still not getting any css when pushing to GH.  Suspect that it may not
    be generating it from the scss.

---
+++ _plugins/sass_convertor.rb
@@ -0,0 +1,20 @@
+module Jekyll
+  require 'sass'
+  class SassConverter < Converter
+    safe true
+    priority :low
+
+     def matches(ext)
+      ext =~ /sass/i
+    end
+
+    def output_ext(ext)
+      ".css"
+    end
+
+    def convert(content)
+      engine = Sass::Engine.new(content)
+      engine.render
+    end
+  end
+end
